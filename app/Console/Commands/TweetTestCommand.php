0865e451e084da35d3e095d19b8fbe4199d16c05
franbarreneche <franbarreneche@gmail.com>
2022-01-16 18:10:25 -0300

    [feature] 5 Command to tweet btc-percentage-completed

--- app/Console/Commands/TweetTestCommand.php
+++ app/Console/Commands/TweetTestCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Twitter;
 
 class TweetTestCommand extends Command
 {
@@ -11,14 +13,14 @@ class TweetTestCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'tweeet:test';
+    protected $signature = 'tweet:btc-percentage-completed';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Tweets a test mesage';
+    protected $description = 'Tweets the percentage of blocks already mined of the total blocks that are needed to mine every one of the 21M btc.';
 
     /**
      * Create a new command instance.
@@ -37,7 +39,34 @@ class TweetTestCommand extends Command
      */
     public function handle()
     {
+        $lastBlock = $this->getLastBlockWithBtc();
+        $currentBlock = $this->getLatestBlock();
 
-        return 0;
+        $percentageCompleted = $this->calcPercentageCompleted($currentBlock, $lastBlock);
+
+        $message = "%$percentageCompleted to the last btc created";
+
+        $this->tweet($message);
+    }
+
+    private function getLatestBlock()
+    {
+        $result = Http::get('https://chain.api.btc.com/v3/block/latest');
+        return $result['data']['height'];
+    }
+
+    private function getLastBlockWithBtc()
+    {
+        return 6930000;
+    }
+
+    private function calcPercentageCompleted(int $currentBlock, int $lastBlock)
+    {
+        return round(($currentBlock / $lastBlock) * 100, 2);
+    }
+
+    private function tweet(string $message)
+    {
+        Twitter::forApiV1()->postTweet(['status' => $message]);
     }
 }
